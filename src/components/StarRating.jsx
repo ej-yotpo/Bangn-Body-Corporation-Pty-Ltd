59f82e8a3cfe3351d3566052a5f50bfa873b608a
ej-yotpo <161460939+ej-yotpo@users.noreply.github.com>
2024-07-31 14:48:57 +0800

    Update StarRating.jsx

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -57,7 +57,7 @@ function StarRating(props) {
                 ></div>
               </section>
 
-              <section>
+              <section style="display:none;">
                 <h1>Collections Star Rating</h1>
                 <div
                   className="yotpo-widget-instance"
