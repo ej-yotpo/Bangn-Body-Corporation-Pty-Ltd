908fa94068bc7b6a6ad46b1c38666f692fbc6832
ej-yotpo <161460939+ej-yotpo@users.noreply.github.com>
2024-08-07 15:38:32 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "";
+  let InstanceId_Widget = "494344";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
